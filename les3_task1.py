8d9ed43ecca40a663141a8d64bde9959d2b03597
PaulLem2019 <tormal@yandex.ru>
2020-06-20 16:21:13 +0300

    Correction based on comments

--- les3_task1.py
+++ les3_task1.py
@@ -10,9 +10,18 @@ def divisor (div1, div2):
 
     return div1/div2
 
+def divisor_v2 (div1, div2):
+    try:
+        rezult = div1/div2
+    except ZeroDivisionError:
+        return 'Деление на ноль не допустимо'
+
+    return rezult
+
 
 numerator = float (input ("Введите значение числителя "))
 denominator = float (input ("Введите значение знаменателя "))
 
 print (f'Деление числитела {numerator} на знаменатель {denominator} равно {divisor(numerator, denominator)}')
 
+print (f'Ver2_Деление числитела {numerator} на знаменатель {denominator} равно {divisor_v2(numerator, denominator)}')
--- les3_task2.py
+++ les3_task2.py
@@ -7,9 +7,18 @@
 def personal_data (first_name, last_name, birthday, residence_place, email, tel_number):
     return first_name+ " " + last_name + " " + birthday + " " + residence_place + " " + email + " " + tel_number
 
+def personal_data_v2 (**kwargs):
+    return kwargs
+
+
 
 dp = personal_data(first_name = "Ivanov", last_name= "Petr", birthday="1985", residence_place="Moscow",
               email="email@mail.ru", tel_number="(495)365-29-45")
 
+dp_v2 = personal_data_v2(first_name = "Ivanov", last_name= "Petr", birthday="1985", residence_place="Moscow",
+              email="email@mail.ru", tel_number="(495)365-29-45")
+
+
 print (dp)
+print (dp_v2)
 
